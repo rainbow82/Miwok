0894b06bd4be2fe3210703b82ccd1535ee739807
Shannon Beach <shannonbeach1982@gmail.com>
2018-03-03 12:49:38 -0500

    updated words array to use arrayList

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,10 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+        Log.v("Numbers Activity", "Word at index 0 " + words.get(0));
+        Log.v("Numbers Activity", "Word at index 1 " + words.get(1));
+        Log.v("Numbers Activity", "Word at index 2 " + words.get(2));
+
     }
+
+
 }
